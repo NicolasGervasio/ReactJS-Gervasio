08926a2bc1cfdd8aaab2c97e9f17c81d1a3fb5b3
Nicolas Gervasio <Nicolasgervasio@outlook.com>
2024-01-09 19:05:40 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return(
+    <div>
+      Hola te amo
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,29 @@
+const header = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <img src="" alt="" />
+                </div>
+                <div className="col">
+                <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
